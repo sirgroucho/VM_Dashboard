5e1cdfc4905a53712c3128a095fc877afd16b12a
Jay Marx <102194316+sirgroucho@users.noreply.github.com>
2025-08-11 00:58:39 -0400

    443 access only

--- app.py
+++ app.py
@@ -4,15 +4,26 @@ from dotenv import load_dotenv
 from routes.auth import auth_bp
 from routes.minecraft import mc_bp
 from datetime import timedelta
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 # Load environment variables from secrets/.env
 load_dotenv(dotenv_path='secrets/.env')
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')  # Read from env file
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
+
 
 app.config['SESSION_PERMANENT'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+
+app.config.update(
+    PREFERRED_URL_SCHEME="https",
+    SESSION_COOKIE_SECURE=True,
+    REMEMBER_COOKIE_SECURE=True,
+)
+
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(mc_bp)
